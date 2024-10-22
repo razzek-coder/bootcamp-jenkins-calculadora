6ba57e5dec71471eeacc74b2feb5b80aae1d3ce5
cesar martinez <razzek.for.mc@gmail.com>
2024-10-21 22:43:27 -0400

    version inicial

---
+++ src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,7 @@
+package calculadora;
+
+public interface Calculadora {
+	
+	int calculate(int i1, char op, int i2);
+
+}
---
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -0,0 +1,10 @@
+package calculadora;
+
+public class CaluladoraImpl implements Calculadora {
+	
+	@Override
+	public int calculate(int i1, char op, int i2) {
+		return 0;
+	}
+
+}
