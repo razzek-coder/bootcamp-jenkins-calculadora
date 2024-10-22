643498cb8d65d713ccb0a299ec84eb6d1150ae00
cesar martinez <razzek.for.mc@gmail.com>
2024-10-21 22:42:54 -0600

    actualizando prueba

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -3,7 +3,7 @@ package calculadora;
 public class CaluladoraImpl implements Calculadora {
 	
 	@Override
-	public int calculate(int i1, char op, int i2) {
+	public int calcular(int i1, char op, int i2) {
 		return 0;
 	}
 
--- src/test/java/calculadora/CaluladoraImplTest.java
+++ src/test/java/calculadora/CaluladoraImplTest.java
@@ -1,14 +1,22 @@
 package calculadora;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CaluladoraImplTest {
 
+	private Calculadora calculadora;
+
+	@BeforeEach
+	void tearUp() {
+		calculadora = new CaluladoraImpl();
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void calcular_sum_success() {
+		int resultado = calculadora.calcular(1, '+', 2);
+		Assertions.assertEquals(3, resultado);
 	}
 
 }
