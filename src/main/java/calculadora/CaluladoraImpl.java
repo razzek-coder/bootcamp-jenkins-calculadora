e93d2ae69bfd995c7b64e94c65dc9fd409a071a5
'cesar <razzek.for.mc@gmail.com>
2024-10-24 16:25:30 -0600

    implementando resta

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -1,10 +1,15 @@
 package calculadora;
 
 public class CaluladoraImpl implements Calculadora {
-	
+
 	@Override
 	public int calcular(int i1, char op, int i2) {
-		return 0;
+		switch (op) {
+		case '-':
+			return i1 - i2;
+		default:
+			return 0;
+		}
 	}
 
 }
