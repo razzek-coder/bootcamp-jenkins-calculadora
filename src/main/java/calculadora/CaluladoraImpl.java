f58adc5e26a47ef9be21a7dd98cdc4f88f423892
JuanCedeTrab <juancedenio7486@gmail.com>
2024-10-24 16:22:14 -0600

    Update CaluladoraImpl.java
    Actualización de suma usando IF-ELSE

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -4,7 +4,11 @@ public class CaluladoraImpl implements Calculadora {
 	
 	@Override
 	public int calcular(int i1, char op, int i2) {
-		return i1 + i2;
+		if (op == '+') {
+                	return i1 + i2;
+	        } else {
+	               return 0;
+	        };
 	}
 
 }
